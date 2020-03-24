2fa54fcd1396e429f95a1d7767ce7ff5f2d3c2c5
Alexis Flores <alex2chivas@hotmail.com>
2020-03-24 15:53:26 -0700

    added applyMiddleware  from redux to hook up redux thunk inside the createstore && created a funnction that return a function inside action creator makign use of redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,35 @@
-import axios from "axios";
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = async() => {
-    const response = await jsonPlaceholder.get('/posts');
+export const fetchPosts = () => async (dispatch) => {
+	const response = await jsonPlaceholder.get('/posts');
 
+	dispatch({
+		type: 'FETCH_POSTS',
+		paylaod: response
+	});
+};
+
+/* This is the same as the function sytax on the top
+export const fetchPosts = () => {
+	return async (dispatch) => {
+		const response = await jsonPlaceholder.get('/posts');
+
+		dispatch({
+			type: 'FETCH_POSTS',
+			paylaod: response
+		});
+	};
+}; */
+
+/* Totally fine to make additional action creators besides the one on the top.
+export const selectPost = () => {
     return {
-        type: 'FETCH_POSTS',
-        payload: response
+        type: 'SLECTED_POSTS',
+        payload: {
+            payment: 20,
+            balance: 100
+        }
     }
 }
 
-
-
-
+*/
