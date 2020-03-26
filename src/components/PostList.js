e3ca5e10bbb7d3697f1ea9e6fcd7c4aacfc762a9
Alexis Flores <alex2chivas@hotmail.com>
2020-03-26 11:33:57 -0700

    added UserHeader as a component to PostList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,23 +2,37 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends Component {
-	componentDidMount() {
+	componentDidMount () {
 		this.props.fetchPosts();
 	}
 
-	render() {
-		console.log(this.props.posts)
-		return <div>Post List</div>
-	};
-};
+	renderList () {
+		return this.props.posts.map(post => {
+			return (
+				<div className='item' key={post.id}>
+					<i className='large middle aligned icon user' />
+					<div className='content'>
+						<div className='description'>
+							<h2>{post.ttle}</h2>
+							<p>{post.body}</p>
+						</div>
+						<UserHeader userId={post.userId} />
+					</div>
+				</div>
+			);
+		});
+	}
 
-const mapStatetoProps = (state) => {
-	return { posts: state.posts.data }
+	render () {
+		return <div className='ui relaxed divided list'>{this.renderList()}</div>;
+	}
+}
+
+const mapStatetoProps = state => {
+	return { posts: state.posts };
 };
 
 export default connect(mapStatetoProps, { fetchPosts })(PostList);
-
-
-
